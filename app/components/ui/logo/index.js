eb3bc7db973c9e51e271ce352393ab20cf2dad9a
Rizer <bashinsky.alexey@gmail.com>
2017-09-11 01:16:41 +0400

    *logo;

--- app/components/ui/logo/index.js
+++ app/components/ui/logo/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from './logo.png';
 
-export default  (props) => (
-    <img src={logo} alt=""/>
+export default (props) => (
+    <Link to="/">
+        <img src={logo} alt=""/>
+    </Link>
 );
